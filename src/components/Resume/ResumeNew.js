5f0a332c4a05282b057e215486f62f73a2aa7111
Your Name <shailendraftp0001@gmail.com>
2022-03-19 13:54:33 +0530

    adde

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "http://localhost:3000/static/media/Soumyajit_Behera-BIT_MESRA.7530381c5cb50cd87371.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
