a1b56640b858b605c2eb953caa30055b036336d8
Your Name <shailendraftp0001@gmail.com>
2022-03-19 13:48:50 +0530

    adde

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Shailendra Kumar </span>
+            from <span className="purple"> Fatehpur , Uttar Pradesh, India.</span>
+            <br />I am an Full Stack developer and special in frontend.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -21,7 +20,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> repairing electronic devices
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -31,7 +30,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Shailendra Kumar</footer>
         </blockquote>
       </Card.Body>
     </Card>
